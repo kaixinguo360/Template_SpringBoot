76fc016633a632013a1f1ea32ae9a1b4a78ec6a3
Kaixinguo <kaixinguo@kaixinguo.site>
2020-01-12 11:30:45 +0800

    First Commit

---
+++ src/main/java/com/my/example/Example.kt
@@ -0,0 +1,4 @@
+package com.my.example
+
+class Example {
+}
---
+++ src/test/java/com/my/example/MyListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
